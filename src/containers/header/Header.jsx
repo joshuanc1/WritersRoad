012d4d8ceda97ee4d6c3faeb1cb0b99c66da9729
Joshua Chu <joshuanc1@gmail.com>
2023-05-05 20:02:28 -0400

    Adjusted Author Card in header page

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import './header.css'
 import {Link} from 'react-router-dom';
+import img from '../../assets/inaki-del-olmo-NIJuEQw0RKg-unsplash.jpg';
+
 const Header = () => {
   return (
     <div className="header_container">
+
       <div className="header_container-card">
         <div className="header-card-info">
           <p>LEARN FROM CRITIQUES</p>
@@ -11,10 +14,20 @@ const Header = () => {
           <p>Collaborate with fellow authors and writers to develop a solid foundation in creating stories successful. Writer's Road can provide insight and creativity towards your next novel.</p>
           <button><Link className="header_link-button" to="/signup">SIGN UP</Link></button>
         </div>
+
         <div className="header-card-author">
-          
+          <div className="header-card-img">
+            <img id="header-img" src={img} alt="header-img" />
+          </div>
+          <div className="header-card-author-info">
+            <h2>Joshua Chu</h2>
+            <p>Aspiring Writer</p>
+            <p>Video Game Nerd</p>
+            <p>Obsessed Developer</p>
+          </div>
         </div>
       </div>
+
     </div>
   )
 }
