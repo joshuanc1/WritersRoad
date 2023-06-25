ab6ddb5d08f10ae85601591694da9d5e87774f90
Joshua Chu <joshuanc1@gmail.com>
2023-06-25 13:49:07 -0400

    appget

--- server/server.js
+++ server/server.js
@@ -22,7 +22,11 @@ app.use(logger);
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname, '..', 'build')));
-    
+
+    app.get("*", (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    })
+
 } else {
     app.use(express.static(path.resolve(__dirname, '..', '/public')));
 }
