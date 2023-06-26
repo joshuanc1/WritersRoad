0afb2b735042a5008a654c33b2e3c80a07d7b92f
Joshua Chu <joshuanc1@gmail.com>
2023-06-26 02:54:46 -0400

    hell

--- server/server.js
+++ server/server.js
@@ -52,7 +52,7 @@ app.use((err, req, res, next) => {
 
   });
 
-  if(process.env.NODE_ENV === 'production') {
+  if(process.env.NODE_ENV == 'production') {
    
     app.use(express.static(path.join(__dirname, 'build')));
 
